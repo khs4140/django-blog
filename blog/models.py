b948906d302fc65a474b61bcb8a6dddd3cef3b38
Hansol Kim <gksthf4140@naver.com>
2022-08-04 02:14:28 +0900

    Post 모델 생성 및 시간 설정

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=30)
+    content = models.TextField()
+
+    created_at = models.DateTimeField(
+        auto_now=True)  # auto_now True 지정시 자동으로 설정됨
+    updated_at = models.DateTimeField(
+        auto_now=True)  # 글 작성시 아예 날짜/시간 정하는 것이 안나옴!
+
+    # author 나중에 작ㅇ
+
+    # admin -> Post 저장된 내용 확일할 수 있도록 __str__ 함수 생성
+    # 어떻게 보여줄지 알려주는듯
+    def __str__(self):
+        return f'[{self.pk}] {self.title}'
--- manage.py
+++ manage.py
@@ -6,7 +6,8 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_blog_proj.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE',
+                          'django_blog_proj.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
