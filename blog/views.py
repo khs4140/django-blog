a2bee904ffd3efa71fa8b0a469157196715c0aec
Hansol Kim <gksthf4140@naver.com>
2022-08-04 21:01:53 +0900

    FBV ->  BLOG post page

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
+from blog.models import Post
+
+from .models import Post
 # Create your views here.
+
+
+def index(request):
+    posts = Post.objects.all().order_by('-pk')
+
+    return render(
+        request,
+        './blog/index.html',
+        {
+            'posts': posts,
+        }
+    )
