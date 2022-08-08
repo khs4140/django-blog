4f142d750041779d2213a2e5ea8fbe1e682e3a0e
Hansol Kim <gksthf4140@naver.com>
2022-08-08 18:31:39 +0900

    요약문 및 미리보기 추가

--- blog/models.py
+++ blog/models.py
@@ -6,6 +6,7 @@ import os
 
 class Post(models.Model):
     title = models.CharField(max_length=30)
+    hook_text = models.CharField(max_length=100, blank=True)
     content = models.TextField()
 
     # 이미지 업로드 blank는 Null 허용
